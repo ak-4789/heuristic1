eeb955a5dc2774f33c43487fbbc948422aeb0a7d
akhilesh pal <akhil@akhileshs-MacBook-Pro.local>
2024-12-28 14:01:36 -0500

    docker test3

--- .github/docker/entrypoint.sh
+++ .github/docker/entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/feed.sh
+python3 /usr/bin/feed.py
 
 git add -A && git commit -m "updated feed"
 git push --set-upstream origin docker-py
